5ff3407b924aedbadb537789de2e092084014ddb
andarusalsa <90945804+andarusalsa@users.noreply.github.com>
2025-05-25 15:50:08 +0000

    up

--- data.js
+++ data.js
@@ -1787,7 +1787,6 @@ const studentData = [
 console.log("Days element:", document.getElementById("days"));
 console.log("Countdown section:", document.getElementById("countdown-section"));
 console.log("Form section:", document.getElementById("form-section"));
-console.log(now, targetDate, distance)
 
 
 document.addEventListener("DOMContentLoaded", function () {
