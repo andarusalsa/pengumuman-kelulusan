f4b5f673df76b43dcb9b531e41fef5331d5275b2
andarusalsa <90945804+andarusalsa@users.noreply.github.com>
2025-05-26 13:32:16 +0000

    update

--- data.js
+++ data.js
@@ -1883,7 +1883,7 @@ document.getElementById("nisn-form").addEventListener("submit", function(e) {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                 </svg>
                 <h3 class="error-title">Data tidak ditemukan</h3>
-                <p class="error-message">Periksa kembali NISN Anda.</p>
+                <p class="error-message">Periksa kembali No. Ujian Anda.</p>
             </div>
         `;
     }
